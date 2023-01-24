6f62efb84b9c20b7d551e775ae266e3b44b3139d
ozgrserkan <ozgr.serkan09@gmail.com>
2023-01-24 13:06:30 +0300

    login page added

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import "../style.scss";
+import { Link } from "react-router-dom";
 
 const Register = () => {
   return (
-    <div>Register</div>
-  )
-}
+    <div className="auth">
+      <h1>Register</h1>
+      <form>
+        <input type="text" placeholder="username" />
+        <input type="password" placeholder="password" />
+        <button>login</button>
+        <span>
+          Don't you have an account ?<Link to="/register"> Register </Link>
+        </span>
+      </form>
+    </div>
+  );
+};
 
-export default Register
+export default Register;
